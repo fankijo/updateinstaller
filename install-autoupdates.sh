6294870f9de49fa1d9fd366576cf2c2cb00423c5
Johannes F <fankijo@users.noreply.github.com>
2020-12-16 16:39:15 +0100

    Create install-autoupdates.sh

---
+++ install-autoupdates.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo Installing updateinstaller...
+echo ""
+
+#download script
+wget -O /bin/updateinstaller https://raw.githubusercontent.com/fankijo/updateinstaller/master/updateinstaller
+
+#make script executable
+chmod +x /bin/updateinstaller
+
+#Install crontjob
+crontab -l > ~/cronjobs
+
+echo "0 4 * * * /bin/updateinstaller" >> ~/cronjobs
+
+crontab ~/cronjobs
+echo "Cronjob installed"
+
+rm ~/cronjobs
