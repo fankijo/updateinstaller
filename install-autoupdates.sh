d93132ea9904c02d02a885e3a579c23d490487bf
Johannes F <fankijo@users.noreply.github.com>
2024-05-02 07:55:16 +0200

    Update install-autoupdates.sh
    added logging

--- install-autoupdates.sh
+++ install-autoupdates.sh
@@ -12,7 +12,7 @@ chmod +x /bin/updateinstaller
 #Install crontjob
 crontab -l > ~/cronjobs
 
-echo "0 4 * * * /bin/updateinstaller" >> ~/cronjobs
+echo '0 4 * * * /bin/updateinstaller >> /var/log/updateinstaller/$(date +"%Y-%m-%d").log' >> ~/cronjobs
 
 crontab ~/cronjobs
 echo "Cronjob installed"
